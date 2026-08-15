819c96102b8e52fbac89c2bef20f321856de8aba
Sayyidusy SA <sijeding15@gmail.com>
2026-08-15 15:29:29 +0700

    chore: ignore .next build cache

--- inventory-dapps/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/node_modules_@stellar_stellar-sdk_dist_stellar-sdk_min_1zl3me7.js","static/chunks/node_modules_0fq3wn0._.js","static/chunks/src_0htbjqt._.js","static/chunks/node_modules_@stellar_08npg0f._.js","static/chunks/src_1ztvc29._.js"],
-    source: "entry"
-});
